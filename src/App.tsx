1b26bc47f52303c38230f35c6d0bedb5c74e105d
DarkAstaroth <mninahuanca4@gmail.com>
2021-07-11 12:15:07 -0400

    Creando una pelicual individual

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Pelicula } from "./Peliculas/peliculas.model";
+import PeliculaIndividual from "./Peliculas/PeliculaIndividual";
 
 function App() {
+  
+  const peliculaPrueba: Pelicula = {
+    id: 1,
+    titulo: "Spider-man",
+    poster:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/spiderman-homecoming-poster-1551691492.jpg",
+  };
+
   return (
     <>
-      <div>hola mundo</div>
+      <PeliculaIndividual pelicula={peliculaPrueba} />
     </>
   );
 }
---
+++ src/Peliculas/PeliculaIndividual.tsx
@@ -0,0 +1,22 @@
+import { Pelicula } from "./peliculas.model";
+import css from './peliculaIndividual.module.css';
+
+export default function PeliculaIndividual(props: peliculaIndividualProps) {
+
+    const construirLink = () => `/pelicula/${props.pelicula.id}`;
+
+    return (
+        <div className={css.div}>
+            <a href={construirLink()}>
+                <img src={props.pelicula.poster} alt="Poster" />
+            </a>
+            <p>
+                <a href={construirLink()}>{ props.pelicula.titulo}</a>
+            </p>
+        </div>
+    );
+}
+
+interface peliculaIndividualProps {
+    pelicula: Pelicula;
+}
